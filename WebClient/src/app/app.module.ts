53b78a1db45fcaeb61d083f9f87e954fbe4b1b3b
Gokul <iamgokulnathanmcs@gmail.com>
2023-10-24 17:21:13 +0530

    ReactiveForms - p1 , old way of input validation

--- WebClient/src/app/app.module.ts
+++ WebClient/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './member/member-list/member-list.component';
@@ -39,6 +39,7 @@ import { PhotoEditorComponent } from './member/photo-editor/photo-editor.compone
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    ReactiveFormsModule,
     FormsModule,
     CommonModules
   ],
--- WebClient/src/app/register/register.component.ts
+++ WebClient/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
+import { AbstractControl, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -13,16 +14,48 @@ export class RegisterComponent {
 
   @Output() RegisterToggle = new EventEmitter();
   model :any ={};
+  registerForm : FormGroup = new FormGroup({}); // cannot declare it as undefined hence initialize it with empty object.
 
   constructor(private accountService :AccountService, private toastr:ToastrService) {
 
   }
 
   ngOnInit() :void {
+    this.initializeForm();
+  }
+
+  // initializing the password.
+  initializeForm(){
+    this.registerForm = new FormGroup({
+      // initial value , [ validation ]
+      username:new FormControl('',Validators.required),
+      password:new FormControl('', [ Validators.required,
+                        Validators.minLength(4), 
+                        Validators.maxLength(8)]),
+      confirmPassword:new FormControl('',[Validators.required,this.matchValues('password')])
+    });
+
+    //we need to preform validation even if user changes initial password.
+    this.registerForm.controls['password'].valueChanges.subscribe({
+      next:()=>{
+      this.registerForm.controls['confirmPassword'].updateValueAndValidity();
+      }
+    });
 
   }
 
+  // comparing password and confirm password
+  matchValues(matchTO :string ):ValidatorFn {
+    return (control:AbstractControl) =>{
+      return control.value === control.parent?.get(matchTO)?.value ? null : {notMatching:true}
+    }
+  }
+
   register() {
+
+    console.log(this.registerForm?.value);
+
+    /*
     console.log(this.model);
     this.accountService.register(this.model).subscribe({
       next: (resp) => {
@@ -31,7 +64,9 @@ export class RegisterComponent {
                       },
       error: (err) => {
       this.toastr.error(err,"Register new user Failed.")}
-    });    
+    });   */ 
+
+
   }
 
   cancel(){
